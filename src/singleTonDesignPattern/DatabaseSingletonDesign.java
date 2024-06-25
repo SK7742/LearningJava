b2c2677e1a112a088c2b3a3895ec48fbb7c34d87
Shivam Kumar <Shivamkumargupta07@gmail.com>
2024-06-25 22:31:44 +0530

    Adding transactions

--- src/singleTonDesignPattern/DatabaseSingletonDesign.java
+++ src/singleTonDesignPattern/DatabaseSingletonDesign.java
@@ -5,9 +5,13 @@ import java.sql.DriverManager;
 
 
 public class DatabaseSingletonDesign {
-	private static Connection mySqlJdbcConnection;
+	public Connection mySqlJdbcConnection;
 	public static DatabaseSingletonDesign databaseSingletonDesign;
+	
 	private DatabaseSingletonDesign() {}
+	public Connection getMySqlJdbcConnection() {
+		return mySqlJdbcConnection;
+	}
 	
 	public static DatabaseSingletonDesign getDatabaseConnection() {
 		if(databaseSingletonDesign == null) {
@@ -17,14 +21,16 @@ public class DatabaseSingletonDesign {
 		return databaseSingletonDesign;
 	}
 
-	private static void establishConnectionWithDataBase(DatabaseSingletonDesign databaseSingletonDesign2) {
-		if(mySqlJdbcConnection == null) {
+	private static void establishConnectionWithDataBase(DatabaseSingletonDesign databaseSingletonDesign) {
+		if(databaseSingletonDesign.mySqlJdbcConnection == null) {
 			try {
 	            // Load the MySQL JDBC driver
 	            Class.forName("com.mysql.cj.jdbc.Driver");
 
 	            // Establish a connection to the database
 	            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/schoolreaderschema", "root", "root");
+	            con.setAutoCommit(false);
+	            databaseSingletonDesign.mySqlJdbcConnection = con;
 	            System.out.println("Connection Established...");
 			}catch(Exception e) {
 				System.out.println("Exception caught: \n" + e.getMessage());
--- src/singleTonDesignPattern/DriverClass.java
+++ src/singleTonDesignPattern/DriverClass.java
@@ -1,8 +1,42 @@
 package singleTonDesignPattern;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
+
 public class DriverClass {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		DatabaseSingletonDesign databaseObj = DatabaseSingletonDesign.getDatabaseConnection();
-		System.out.println(databaseObj);
+		Connection connection = databaseObj.getMySqlJdbcConnection();
+		try {
+			connection.beginRequest();
+			PreparedStatement statement = connection.prepareStatement("insert into student values (?, ?, ?, ?, ?, ?, ?)");
+			statement.setInt(1, UUID.randomUUID().hashCode());
+			statement.setString(2, "Test Name One");
+			statement.setDate(3, new Date(1998, 10, 15));
+			statement.setString(4, "O+");
+			statement.setString(5, "Test Father's Name One");
+			statement.setString(6, "Test Mother's Name One");
+			statement.setLong(7, 91876543210L);
+			statement.executeUpdate();
+			
+			PreparedStatement statementTwo = connection.prepareStatement("insert into student values (?, ?, ?, ?, ?, ?, ?)");
+			statementTwo.setInt(1, UUID.randomUUID().hashCode());
+			statementTwo.setString(2, "Test Name Two");
+			statementTwo.setDate(3, new Date(2000, 9, 15));
+			statementTwo.setString(4, "O-");
+			statementTwo.setString(5, "Test Father's Name Two");
+			statementTwo.setString(6, "Test Mother's Name Two");
+			statementTwo.setLong(7, 8967453210L);
+			statementTwo.executeUpdate();
+			connection.commit();
+			System.out.println("Data inserted...");
+		}catch(Exception e) {
+			connection.rollback();
+			System.out.println("Txn rollback...");
+			System.out.println(e.getMessage());
+		}
 	}
 }
